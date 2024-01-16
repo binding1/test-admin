6c4304cd3024626bc8313eb931147bc1b24b5241
Jaewon Bae <jaey.pro@gmail.com>
2024-01-16 16:54:55 -0500

    add dashboard and login

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Admin, Resource, ShowGuesser} from "react-admin";
 import { dataProvider } from './dataProvider';
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./components/posts";
+import { Dashboard } from "./components/Dashboard";
+import { authProvider } from "./authProvider";
 
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
 
-export const App = () => <Admin dataProvider={dataProvider}>
+export const App = () => <Admin authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard}>
   <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
   <Resource name="users" list={UserList} show={ShowGuesser} recordRepresentation="name" icon={UserIcon}/>
 </Admin>;
