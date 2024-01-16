ef8285a48665c5c8fb832930208e0ac064ca64a8
Jaewon Bae <jaey.pro@gmail.com>
2024-01-16 16:41:02 -0500

    add edit for post list

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -1,13 +1,25 @@
 // in src/posts.tsx
-import { List, Datagrid, TextField, ReferenceField, EditButton } from "react-admin";
+import { List, Datagrid, TextField, ReferenceField, EditButton, Edit, SimpleForm, TextInput, ReferenceInput } from "react-admin";
 
 export const PostList = () => (
     <List>
         <Datagrid>
             <TextField source="id" />
-            <ReferenceField source="userId" reference="users" />    
+            <ReferenceField source="userId" reference="users" link="show" />    
             <TextField source="title" />
             <EditButton />
         </Datagrid>
     </List>
+);
+
+
+export const PostEdit = () => (
+    <Edit>
+        <SimpleForm>
+        <TextInput source="id" InputProps={{ disabled: true }} />
+            <ReferenceInput source="userId" reference="users" />
+            <TextInput source="title" />
+            <TextInput source="body" multiline rows={5} />
+        </SimpleForm>
+    </Edit>
 );
