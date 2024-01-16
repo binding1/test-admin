cb0112061ace1f6b1e215a341a9876e75f3144ec
Jaewon Bae <jaey.pro@gmail.com>
2024-01-16 16:21:14 -0500

    change website column to url field

--- src/users.tsx
+++ src/users.tsx
@@ -1,5 +1,5 @@
 import { useMediaQuery, Theme } from "@mui/material";
-import { List, SimpleList, Datagrid, TextField, EmailField } from "react-admin";
+import { List, SimpleList, Datagrid, TextField, EmailField, UrlField } from "react-admin";
 
 export const UserList = () => {
     const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
@@ -15,11 +15,9 @@ export const UserList = () => {
                 <Datagrid rowClick="edit">
                     <TextField source="id" />
                     <TextField source="name" />
-                    <TextField source="username" />
                     <EmailField source="email" />
-                    <TextField source="address.street" />
                     <TextField source="phone" />
-                    <TextField source="website" />
+                    <UrlField source="website" />
                     <TextField source="company.name" />
                 </Datagrid>
             )}
